627a0e42b4a7e19e4c82043c6a586a046413900e
JacksonStormZz <jassadawut.2007@gmail.com>
2025-10-15 06:57:40 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,28 +3,28 @@ var product = [{
     img: 'https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/492235189_122153336216543947_8853667336589421896_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Xjp7CLwfifoQ7kNvwGS5aNm&_nc_oc=Adn3jzAJFfR564LOAtlMYcbMbaakXQ_RsK6_DOcqSsGJnhhLqGIHFTBIKbrRtFkm9mGUzDMipsIA0OF8NqsSv688&_nc_zt=23&_nc_ht=scontent.fkkc3-1.fna&_nc_gid=R8Hw1MB5VZdGrI0eL2icRg&oh=00_AfcCaWQ5Zl7Dihnh4L6xkjqKgkHF-R5cuYy6IH3IuvoH2A&oe=68F3F72E',
     name: 'Nashville Hot Chicken',
     price: 69,
-    description: 'Nike Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    description: 'spisy',
     type: 'chicken'
 }, {
     id: 2,
     img: 'https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/475461889_122135832356543947_6736248945548261388_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=KxZlUX6QH5IQ7kNvwEHyxJU&_nc_oc=AdkZ9LXYPL1_C_QwpQWljJ_0Cy2LeN3QdrrU1CkBvo7TocydleVjHVzl_DvFNai8TNug7bovgREGuiLF5Ox72VCK&_nc_zt=23&_nc_ht=scontent.fkkc3-1.fna&_nc_gid=H2-_LIm0gpZ1tLZLzyrQeA&oh=00_Afea-AvAibJWVl7bce06kpVklsUYfFKC1RDulNkebZQ9Yw&oe=68F3FF88',
     name: 'Chicken Wrap',
     price: 50,
-    description: 'Adidas shirt Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    description: 'juisy
     type: 'wrap'
 }, {
     id: 3,
     img: 'https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/480486545_122139171728543947_2342050265841889652_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=1kvl7gurLbsQ7kNvwFZCUxa&_nc_oc=AdkJDiMVh7t6K3QSgbaoRC1ojKrmtQBi1nnQ3nvPRAm5MPKpfQjO3uBqeuko5yTo2GGNYWFWDzoT4Uam8y5ZcbFW&_nc_zt=23&_nc_ht=scontent.fkkc3-1.fna&_nc_gid=r_nKv2V1meGCYYDr6V33_g&oh=00_AfcD018vxGgfz_VoYXi48IFAw1wcR0i2j_leytJBJSTLSw&oe=68F3FF76',
     name: 'Pizza roll',
     price: 60,
-    description: 'Adidas shoe Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    description: 'yummy',
     type: 'wrap'
 }, {
     id: 4,
     img: 'https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/495155372_122156483534543947_3101379461614384793_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=fWf95jJWIOcQ7kNvwEGgQCQ&_nc_oc=AdlAE4LiM0Stn3A9vtH6h_K7J9YQ2vuVgR3Vxev6hsqGpHMZiUu1v21XDSi7nNsC0iuRb-cHlhutUg4k24FDvh14&_nc_zt=23&_nc_ht=scontent.fkkc3-1.fna&_nc_gid=iHl0LH0j9VOZsuLbAXq5kQ&oh=00_AfeJlzi93Y0awiMcRZsIWDN8k0RUq6H5Nl0IPylryYaviw&oe=68F3FCFA',
     name: 'Fried Chicken fish sauce',
     price: 50,
-    description: 'Adidas shoe Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam, labore dolorum optio ad consequatur cupiditate!',
+    description: 'yummy',
     type: 'chicken'
 }];
 
