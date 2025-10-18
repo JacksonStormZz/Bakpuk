7d4f2bfa56ba477265f241fc811d8782660965f5
JacksonStormZz <jassadawut.2007@gmail.com>
2025-10-18 20:49:48 +0700

    Fix spelling errors and update product rendering
    Correct spelling errors in product descriptions and adjust AJAX call for product list rendering.

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ var product = [{
     img: 'https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/492235189_122153336216543947_8853667336589421896_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=Xjp7CLwfifoQ7kNvwGS5aNm&_nc_oc=Adn3jzAJFfR564LOAtlMYcbMbaakXQ_RsK6_DOcqSsGJnhhLqGIHFTBIKbrRtFkm9mGUzDMipsIA0OF8NqsSv688&_nc_zt=23&_nc_ht=scontent.fkkc3-1.fna&_nc_gid=R8Hw1MB5VZdGrI0eL2icRg&oh=00_AfcCaWQ5Zl7Dihnh4L6xkjqKgkHF-R5cuYy6IH3IuvoH2A&oe=68F3F72E',
     name: 'Nashville Hot Chicken',
     price: 69,
-    description: 'yummy',
+    description: 'yuumy',
     type: 'chicken'
 }, {
     id: 2,
     img: 'https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/475461889_122135832356543947_6736248945548261388_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=KxZlUX6QH5IQ7kNvwEHyxJU&_nc_oc=AdkZ9LXYPL1_C_QwpQWljJ_0Cy2LeN3QdrrU1CkBvo7TocydleVjHVzl_DvFNai8TNug7bovgREGuiLF5Ox72VCK&_nc_zt=23&_nc_ht=scontent.fkkc3-1.fna&_nc_gid=H2-_LIm0gpZ1tLZLzyrQeA&oh=00_Afea-AvAibJWVl7bce06kpVklsUYfFKC1RDulNkebZQ9Yw&oe=68F3FF88',
     name: 'Chicken Wrap',
     price: 50,
-    description: 'yummy',
+    description: 'ymumy',
     type: 'wrap'
 }, {
-    id: 3,
-    img: 'https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/480486545_122139171728543947_2342050265841889652_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=1kvl7gurLbsQ7kNvwFZCUxa&_nc_oc=AdkJDiMVh7t6K3QSgbaoRC1ojKrmtQBi1nnQ3nvPRAm5MPKpfQjO3uBqeuko5yTo2GGNYWFWDzoT4Uam8y5ZcbFW&_nc_zt=23&_nc_ht=scontent.fkkc3-1.fna&_nc_gid=r_nKv2V1meGCYYDr6V33_g&oh=00_AfcD018vxGgfz_VoYXi48IFAw1wcR0i2j_leytJBJSTLSw&oe=68F3FF76',
+   id: 3,
+   img: 'https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/480486545_122139171728543947_2342050265841889652_n.jpg?_nc_cat=107&ccb=1-7&_nc_sid=833d8c&_nc_ohc=1kvl7gurLbsQ7kNvwFZCUxa&_nc_oc=AdkJDiMVh7t6K3QSgbaoRC1ojKrmtQBi1nnQ3nvPRAm5MPKpfQjO3uBqeuko5yTo2GGNYWFWDzoT4Uam8y5ZcbFW&_nc_zt=23&_nc_ht=scontent.fkkc3-1.fna&_nc_gid=r_nKv2V1meGCYYDr6V33_g&oh=00_AfcD018vxGgfz_VoYXi48IFAw1wcR0i2j_leytJBJSTLSw&oe=68F3FF76',
     name: 'Pizza roll',
     price: 60,
     description: 'yummy',
@@ -24,13 +24,35 @@ var product = [{
     img: 'https://scontent.fkkc3-1.fna.fbcdn.net/v/t39.30808-6/495155372_122156483534543947_3101379461614384793_n.jpg?stp=cp6_dst-jpg_tt6&_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_ohc=fWf95jJWIOcQ7kNvwEGgQCQ&_nc_oc=AdlAE4LiM0Stn3A9vtH6h_K7J9YQ2vuVgR3Vxev6hsqGpHMZiUu1v21XDSi7nNsC0iuRb-cHlhutUg4k24FDvh14&_nc_zt=23&_nc_ht=scontent.fkkc3-1.fna&_nc_gid=iHl0LH0j9VOZsuLbAXq5kQ&oh=00_AfeJlzi93Y0awiMcRZsIWDN8k0RUq6H5Nl0IPylryYaviw&oe=68F3FCFA',
     name: 'Fried Chicken fish sauce',
     price: 50,
-    description: 'yummy',
+   description: 'yummy',
     type: 'chicken'
 }];
 
 // [{},{},{}] // length = 3
+var product;
 
-$(document).ready(() => {
+$(document).ready(() => { 
+    $.ajax({
+        method:'get',
+        url:'./api/getallproduct.php',
+        success: function(response) {
+            console.log(response)
+            if(response.RespCode== 200){
+                product = response.Result;
+                 var html = '';
+                    for (let i = 0; i < product.length; i++) {
+                        html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                                    <img class="product-img" src="${product[i].img}" alt="">
+                                    <p style="font-size: 1.2vw;">${product[i].name}</p>
+                                    <p stlye="font-size: 1vw;">${ numberWithCommas(product[i].price) } THB</p>
+                                </div>`;
+                    }
+    $("#productlist").html(html);
+            }
+        },error: function(err) {
+            console.log(err)
+        }
+    })
     var html = '';
     for (let i = 0; i < product.length; i++) {
         html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
@@ -206,4 +228,52 @@ function deinitems(action, index) {
         rendercart();
     }
 }
+// เพิ่มฟังก์ชันนี้ในไฟล์ index.js
+function checkout() {
+    if (cart.length === 0) {
+        Swal.fire({
+            icon: 'error',
+            title: 'ตะกร้าว่างเปล่า!',
+        });
+        return;
+    }
 
+    // ในระบบจริง คุณต้องมี Modal/Form ให้ลูกค้ากรอกชื่อ ที่อยู่ เบอร์โทรศัพท์ ก่อน
+    // ส่วนนี้คือการจำลองการส่งข้อมูลในตัวแปร 'cart' ไปยัง Backend
+    
+    var orderData = {
+        // อาจจะต้องเพิ่มข้อมูลลูกค้า เช่น name: 'John Doe', address: '123 Main St'
+        items: cart,
+        total: cart.reduce((sum, item) => sum + (item.price * item.count), 0),
+    };
+
+    // ส่งข้อมูลไปบันทึกที่ Backend (ต้องสร้างไฟล์ checkout.php ใน Backend)
+    $.ajax({
+        method: 'post',
+        url: './api/checkout.php',
+        data: orderData,
+        success: function(response) {
+            // สมมติว่า Backend ตอบกลับว่าสำเร็จ
+            Swal.fire({
+                icon: 'success',
+                title: 'สั่งซื้อสำเร็จ!',
+                text: 'รายการสั่งซื้อของคุณถูกส่งไปยังห้องครัวแล้ว',
+                showConfirmButton: false,
+                timer: 2000
+            });
+            // ล้างตะกร้าและอัปเดต UI
+            cart = [];
+            closeModal();
+            $("#cartcount").css('display','none');
+            rendercart(); 
+        },
+        error: function(err) {
+            Swal.fire({
+                icon: 'error',
+                title: 'สั่งซื้อล้มเหลว',
+                text: 'ไม่สามารถทำรายการสั่งซื้อได้',
+            });
+            console.log(err);
+        }
+    });
+}
